557774094dcc5d3998188a18ac2bef6ae8fbfb56
thor <just.thor@gmail.com>
2021-08-23 21:21:37 +0800

    added scheduled task to populate character data from remote api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CharacterModule } from './modules/character/character.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CharacterModule],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), CharacterModule],
 })
 export class AppModule {}
--- src/modules/character/character.module.ts
+++ src/modules/character/character.module.ts
@@ -1,8 +1,10 @@
-import { CacheModule, Module, OnModuleInit } from '@nestjs/common';
+import { CacheModule, Logger, Module, OnModuleInit } from '@nestjs/common';
 import { CharacterService } from './character.service';
 import { CharacterController } from './character.controller';
 import { CharacterExternalApi } from './character.external.api';
 import { CharacterTask } from './character.task';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 @Module({
   providers: [CharacterService, CharacterExternalApi, CharacterTask],
@@ -10,8 +12,22 @@ import { CharacterTask } from './character.task';
   controllers: [CharacterController],
 })
 export class CharacterModule implements OnModuleInit {
-  constructor(private readonly characterTask: CharacterTask) {}
+  private logger = new Logger(CharacterModule.name);
+
+  constructor(
+    private readonly characterTask: CharacterTask,
+    private schedulerRegistry: SchedulerRegistry,
+  ) {}
   async onModuleInit(): Promise<any> {
-    await this.characterTask.populateCharacters();
+    // await this.characterTask.populateCharacters();
+    this.logger.log('character module init');
+    const milliseconds =
+      (parseInt(process.env.POPULATE_CHARACTERS_INTERVAL) || 60) * 1000;
+    const populateCharacterCallback = async () => {
+      await this.characterTask.populateCharacters();
+    };
+    await populateCharacterCallback();
+    const interval = setInterval(populateCharacterCallback, milliseconds);
+    this.schedulerRegistry.addInterval('populate-characters', interval);
   }
 }
